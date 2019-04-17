26c7be811f69d1d5d4af1f5fe3f8911b4de2f362
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-04-17 03:31:14 -0500

    Create rec.cs
    Adding cs version

---
+++ rec.cs
@@ -0,0 +1,45 @@
+// Jacob Wilkins
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class rec
+{
+  static public void processToken( string token ) {
+
+    Regex idPattern1 = new Regex(@"^([$]|[|]|[a-z])[A-Z0-9]*$", RegexOptions.Compiled);
+    Regex idPattern2 = new Regex(@"^([|][A-Z0-9]*[|])$", RegexOptions.Compiled);
+    Regex intPattern = new Regex(@"^(%)([0-9]|[A-Fa-f])+$", RegexOptions.Compiled);
+    Regex fpPattern = new Regex(@"^[0-9]+[.][0-9]+$", RegexOptions.Compiled);
+    
+    Match idMatch1 = idPattern1.Match(token);
+    Match idMatch2 = idPattern2.Match(token);
+    Match intMatch = intPattern.Match(token);
+    Match fpMatch = fpPattern.Match(token);
+
+    if (idMatch1.Success) {
+      Console.WriteLine(">" + token + "< matches ID");
+    } else if (idMatch2.Success) {
+      Console.WriteLine(">" + token + "< matches ID");
+    } else if (intMatch.Success) {
+      Console.WriteLine(">" + token + "< matches INT");
+    } else if (fpMatch.Success) {
+      Console.WriteLine(">" + token + "< matches FP");
+    } else {
+      Console.WriteLine(">" + token + "< does not match");
+    }
+
+  }
+
+  static public void Main( string[] args )
+  {
+    Console.WriteLine( "processing tokens from " + args[ 0 ] + " ..." );
+
+    foreach ( string line in File.ReadAllLines( args[ 0 ] ) ) {
+      foreach ( string token in line.Split( (char []) null, StringSplitOptions.RemoveEmptyEntries ) ) {
+        processToken( token );
+      }
+    }
+  }
+}
