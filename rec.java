de4034cbe9a156b90e6ff350df5705bc4ae66948
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-04-17 03:33:02 -0500

    Create rec.java
    Adding java version

---
+++ rec.java
@@ -0,0 +1,52 @@
+// Jacob Wilkins
+
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.regex.*;
+
+public class rec {
+  public static void processToken( String token )
+  {
+    String idPattern1 = "^([$]|[|]|[a-z])[A-Z0-9]*$";
+    String idPattern2 = "^([|][A-Z0-9]*[|])$";
+    String intPattern = "^(%)([0-9]|[A-Fa-f])+$";
+    String fpPattern = "^[0-9]+[.][0-9]+$";
+
+    Pattern idRegex1 = Pattern.compile(idPattern1);
+    Pattern idRegex2 = Pattern.compile(idPattern2);
+    Pattern intRegex = Pattern.compile(intPattern);
+    Pattern fpRegex = Pattern.compile(fpPattern);
+
+    Matcher idMatcher1 = idRegex1.matcher(token);
+    Matcher idMatcher2 = idRegex2.matcher(token);
+    Matcher intMatcher = intRegex.matcher(token);
+    Matcher fpMatcher = fpRegex.matcher(token);
+
+    if (idMatcher1.matches()) {
+      System.out.println(">" + token + "< matches ID");
+    } else if (idMatcher2.matches()) {
+      System.out.println(">" + token + "< matches ID");
+    } else if (intMatcher.matches()) {
+      System.out.println(">" + token + "< matches INT");
+    } else if (fpMatcher.matches()) {
+      System.out.println(">" + token + "< matches FP");
+    } else {
+      System.out.println(">" + token + "< does not match");
+    }
+
+  }
+
+  public static void main( String[] args )
+  {
+    System.out.println( "processing tokens from " + args[ 0 ] + " ..." );
+
+    try {
+      Files.lines( Paths.get( args[ 0 ] ) ).forEachOrdered(
+        line -> Arrays.stream( line.split( "\\s+" )  )
+            .forEachOrdered( token -> processToken( token ) ) );
+    } catch ( java.io.IOException e ) {
+      e.printStackTrace();
+    }
+  }
+}
